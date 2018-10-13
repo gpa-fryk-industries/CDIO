c20a478be72f5855585d5129cb25e0fd93275275
davfr422 <davfr422@student.liu.se>
2018-10-13 15:19:33 +0200

    ADPCM code update.
    Made some slight changes in the VHDL code, specifically the adpcm parts. Also added a few comments.

--- codec/FPGA_DE1/adpcm_adapt.vhd
+++ codec/FPGA_DE1/adpcm_adapt.vhd
@@ -2,6 +2,8 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+-- This looks stupid but seems like the only way to implement generic arrays, if needed. At the moment these arrays (LUTs) are implemented 
+-- as signals. 
 
 package mytypes_pkg_3 is
 
@@ -18,36 +20,38 @@ entity adpcm_adapt is
 	
 	generic (
 		
-		N : integer := 2
+		N : integer := 2 -- How many stages of ADPCM do we want? N = 1 -> 2 bit, N = 2 -> 3 bit and N = 3 -> 4 bit compression.
 	
 	);
 	
    port( 
 	
-		Code_in     : in unsigned(N downto 0);
-		Diffq_in    : in signed(15 downto 0);
-		Sample_in   : in  signed(7 downto 0);
+		Code_in     : in unsigned(N downto 0); -- Encoded signal. 
+		Diffq_in    : in signed(15 downto 0); -- Diffq signal from inverted quantizer.
+		Sample_in   : in  signed(7 downto 0); -- New sample from microphone / sound source. 
 	
-		Clock : in std_logic;
-		Reset : in std_logic;
+		Clock       : in std_logic; -- Clock signal. 8 KHz in the moment of writing this.
+		Reset       : in std_logic; -- Reset signal. Reset module on '1'. 
 		
-		y_code      : out unsigned(N downto 0);
-		y_signal		: out signed(7 downto 0);
+		--y_code      : out unsigned(4 downto 0); -- Encoded output signal, might drop this later on.
+		y_signal	: out signed(7 downto 0); -- Decoded output signal in decoder mode.
 		
-		Diff_out    : out signed(15 downto 0);
-		Step_out    : out signed(15 downto 0)
+		Diff_out    : out signed(15 downto 0); -- Difference between sample and predicted sample. Send to Quantizer.
+		Step_out    : out signed(15 downto 0) -- Step signal to Quantizer and inverted Quantizer.
                   
 	);
 end adpcm_adapt;
 
 architecture arch_adpcm_adapt of adpcm_adapt is
 	
-	signal pred_sample     : signed(15 downto 0) := (others => '0');
-	signal pred_sample_out : signed(15 downto 0) := (others => '0');
-	signal pred_index      : integer := 0;
+	signal pred_sample     : signed(15 downto 0) := (others => '0'); -- Predicted sample for next cycle. Initialize as 0.
+	signal pred_index      : integer := 0; -- Predicted index for next cycle. Initialize as 0.
 	
-	signal sample_padded : signed(15 downto 0);
+	signal sample_padded : signed(15 downto 0); -- Padded sample, algorithm is working with 16 bit numbers in this implementation. 
+												-- Input signal scaled to 16 bit.
 	
+	
+	-- Look up table for generating step signal to Quantizer and inverted Quantizer.
 	signal	StepSizeTable : my_array_t1(integer range 0 to 88) := (7, 8, 9, 10, 11, 12, 13, 14, 16, 17,
 												19, 21, 23, 25, 28, 31, 34, 37, 41, 45,
 												50, 55, 60, 66, 73, 80, 88, 97, 107, 118,
@@ -58,8 +62,9 @@ architecture arch_adpcm_adapt of adpcm_adapt is
 												5894, 6484, 7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899,
 												15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767);
 												
-	signal IndexTable	  : my_array_t1(integer range 0 to 7) := (-1, -1,  2, 6,  -1, -1,  2, 6); 
-																									-- Make generic later!				
+	-- Look up table to generate index in StepSizeTable.											
+	signal IndexTable	  : my_array_t1(integer range 0 to 15) := (-1, -1, -1, -1 , 2, 4, 6, -1, -1, -1, -1, 2, 4, 6, 8); 
+																											
 	
 begin   
 
@@ -67,27 +72,35 @@ begin
 	
 	begin
 		
+		-- Go back to initial "state".
 		if (Reset = '1') then
 		
-			pred_sample <= (others => '0');
+			pred_sample <= (others => '0'); 
 			pred_index <= 0;
-			
+		
+		-- 
 		elsif ( rising_edge(Clock) ) then
 			
-			if(Code_in(N) = '1') then
-			
+			-- Is the sign of Code_in negative / is MSB '1'?
+			if(Code_in(N) = '1') then		
 				pred_sample <= pred_sample - Diffq_in;	
 			else
 				pred_sample <= pred_sample +  Diffq_in;
 			end if;
 			
 			
-			if( pred_index + IndexTable(to_integer( Code_in)) < 0 ) then
+			--Instead of making IndexTable generic, index prediction is adapted to the number N.
+			--(-1, -1, -1, -1,  2, 4, 6, -1, -1, -1, -1, 2, 4, 6, 8) if N = 3
+			--(-1, -1, 2, 6, -1, -1, 2, 6)                         if N = 2
+			--(-1, 2,-1, 2)                                        if N = 1
+			
+			-- to_integer( Code_in ) * (2**(3-N) might just look a bit cryptic...
+			if( pred_index + IndexTable(to_integer( Code_in ) * (2**(3-N)) ) < 0 ) then
 				pred_index <= 0;
-			elsif( pred_index + IndexTable(to_integer( Code_in )) > 88 ) then
+			elsif( pred_index + IndexTable(to_integer( Code_in ) * (2**(3-N)) ) > 88 ) then
 				pred_index <= 88;	
 			else
-				pred_index <= pred_index + IndexTable( to_integer(Code_in) );
+				pred_index <= pred_index + IndexTable( to_integer(Code_in) * (2**(3-N)) ) ;
 			end if;
 			
 		end if;
@@ -95,6 +108,8 @@ begin
 		
 	end process;
 	
+	
+	-- Below is code to prevent overflow/underflow. Not yet tested so kept commented out at the moment.
 	
 --	process(pred_sample) begin
 --	
@@ -113,15 +128,25 @@ begin
 --	
 --	end process;
 	
-	Sample_padded <= Sample_in & "00000000";
+	Sample_padded <= Sample_in & "00000000"; -- Scale input signal to 16 bit.
 	
 
-	Diff_out <=  ( Sample_padded - pred_sample );
-	Step_out <= to_signed(StepSizeTable(pred_index) , 16 );
+	Diff_out <=  ( Sample_padded - pred_sample ); 
+	Step_out <= to_signed(StepSizeTable(pred_index) , 16 ); 
 	
-	y_signal <= pred_sample(15 downto 8);
+	y_signal <= pred_sample(15 downto 8); -- Scale back output as an 8 bit number.
 	
-	y_code <= Code_in and to_unsigned((2**(N+1) -1 ) ,N+1);
+	
+	-- This part might not be necessary since length of Code_in is generic in its declaration. Keep it if length of encoded should be 4 bits regardless
+	-- of how we encode.
+	
+	-- y_code <= Code_in and to_unsigned(3 ,4); 
+	-- y_code <= Code_in and to_unsigned(7 ,4); 
+	-- y_code <= Code_in and to_unsigned(15 ,4); 
+	
+	-- y_code <= Code_in and to_unsigned((2**(N+1) -1 ), 4); 
+	
+	
 	
 end arch_adpcm_adapt;
 
--- codec/FPGA_DE1/adpcm_inv_quantizer.vhd
+++ codec/FPGA_DE1/adpcm_inv_quantizer.vhd
@@ -12,28 +12,29 @@ entity adpcm_inv_quantizer is
 	
    port( 
 		
-		Code_in   : in unsigned(N downto 0);
-		Step_in   : in signed(15 downto 0);		
-		Diffq_out : out signed(15 downto 0)
+		Code_in   : in unsigned(N downto 0); -- Encoded signal 
+		Step_in   : in signed(15 downto 0);	-- Step signal from LUT StepSizeTable. See adpcm_adapt.vhd...
+		Diffq_out : out signed(15 downto 0) -- Diffq output to adapt stage.
                   
 	);
 end adpcm_inv_quantizer;
 
 architecture arch_adpcm_inv_quantizer of adpcm_inv_quantizer is
 	
+	-- Might just drop one of these array types since they are identical.
 	type diffq_array is array (0 to N) of signed(15 downto 0);
 	type step_array is array (0 to N) of signed(15 downto 0);
 	
-	signal diffq : diffq_array;
-	signal step : step_array;
+	signal diffq : diffq_array; --internal diffq signal.
+	signal step : step_array; --internal step signal.
 	
 begin
 
-	diffq(0) <= Step_in / 8;
-	step(0) <= Step_in /  (2**(3-N));
+	diffq(0) <= Step_in / 8; -- Bitshift 3 times. 
+	step(0) <= Step_in /  (2**(3-N)); --Initalize step differently depending on N, i.e. bitshift Step_in differently. 
 	
-	QUANTIZER: process(Code_in, Step_in) is 
-	
+	-- Bring it. 
+	INV_QUANTIZER: process(Code_in, Step_in) is 
 	begin
 	
 		ADPCM_INV_QUANTIZER_GEN: for i in 0 to N-1 loop 
@@ -48,13 +49,13 @@ begin
 			
 			end if;
 				
-			step(i+1) <= step(i) / 2;
+			step(i+1) <= step(i) / 2; --Bit shift to the next stage.
 			
 		end loop;
 		
 	end process;
 		
-	Diffq_out <= diffq(N);
+	Diffq_out <= diffq(N); -- Set Diffq output to the Nth value in diffq array.
 
 end arch_adpcm_inv_quantizer;
 
