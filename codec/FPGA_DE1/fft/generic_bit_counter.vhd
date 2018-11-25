c7045f8c457ae70c7bc0dcbe02d975294e8ffaf2
David Frykskog <davfr422@student.liu.se>
2018-11-25 18:34:54 +0100

    Added VHDL code to codec

--- codec/FPGA_DE1/fft/generic_bit_counter.vhd
+++ codec/FPGA_DE1/fft/generic_bit_counter.vhd
@@ -3,24 +3,18 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity test_counter is
-	
-	generic (
-	
-		N_stages : integer := 4
-	
-	);
+
+
 	
 	port( 
 	
 		clock : in std_logic;
 		reset : in std_logic;
 		
-		sel0  : out std_logic;
-		sel1  : out std_logic;
-		sel2  : out std_logic;
-		sel3  : out std_logic;
-		
-		count_debug : out unsigned(3 downto 0)
+		sel0   : out std_logic;
+		sel1   : out std_logic;
+		sel2   : out std_logic;
+		sel3   : out std_logic
 
 			
 	);
@@ -37,12 +31,11 @@ begin
 	begin
 		
 		if reset = '1' then
-		
 				count <= (others => '0');
 
 		elsif rising_edge(clock) then
 		
-			if count >= to_unsigned( (2**N_stages)-1, 4) then
+			if count >= to_unsigned(15, 4) then
 		
 				count <= (others => '0');
 			else
@@ -55,12 +48,11 @@ begin
 	
 	end process;
 	
+	
 	sel0 <= count(0);
 	sel1 <= count(1);
 	sel2 <= count(2);
 	sel3 <= count(3);
-	
-	count_debug <= count;
 
 end arch_test_counter;
 	
