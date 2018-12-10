9111d5c3dc2daf61e8f60f9e346fc5540e35c24a
Atmelfan <gustavp@gpa-robotics.com>
2018-12-11 00:09:58 +0100

    Added missing codes...

--- fw/mcu/src/radio/spirit1.c
+++ fw/mcu/src/radio/spirit1.c
@@ -2,11 +2,41 @@
 // Created by atmelfan on 2018-11-25.
 //
 
+/**
+ * Based on mbed-spirit1-example
+ * Here: https://github.com/ubirch/mbed-spirit1-example
+ */
+
 #include <stdio.h>
 #include "spirit1.h"
 #include "SPIRIT1_Library/Inc/SPIRIT_Radio.h"
 #include "SPIRIT1_Library/Inc/SPIRIT_Config.h"
 
+
+/* list of the command codes of SPIRIT1 */
+#define    COMMAND_TX                                          ((uint8_t)(0x60)) /*!< Start to transmit; valid only from READY */
+#define    COMMAND_RX                                          ((uint8_t)(0x61)) /*!< Start to receive; valid only from READY */
+#define    COMMAND_READY                                       ((uint8_t)(0x62)) /*!< Go to READY; valid only from STANDBY or SLEEP or LOCK */
+#define    COMMAND_STANDBY                                     ((uint8_t)(0x63)) /*!< Go to STANDBY; valid only from READY */
+#define    COMMAND_SLEEP                                       ((uint8_t)(0x64)) /*!< Go to SLEEP; valid only from READY */
+#define    COMMAND_LOCKRX                                      ((uint8_t)(0x65)) /*!< Go to LOCK state by using the RX configuration of the synth; valid only from READY */
+#define    COMMAND_LOCKTX                                      ((uint8_t)(0x66)) /*!< Go to LOCK state by using the TX configuration of the synth; valid only from READY */
+#define    COMMAND_SABORT                                      ((uint8_t)(0x67)) /*!< Force exit form TX or RX states and go to READY state; valid only from TX or RX */
+#define    COMMAND_SRES                                        ((uint8_t)(0x70)) /*!< Reset of all digital part, except SPI registers */
+#define    COMMAND_FLUSHRXFIFO                                 ((uint8_t)(0x71)) /*!< Clean the RX FIFO; valid from all states */
+#define    COMMAND_FLUSHTXFIFO                                 ((uint8_t)(0x72)) /*!< Clean the TX FIFO; valid from all states */
+
+#define HEADER_WRITE_MASK     0x00 /*!< Write mask for header byte*/
+#define HEADER_READ_MASK      0x01 /*!< Read mask for header byte*/
+#define HEADER_ADDRESS_MASK   0x00 /*!< Address mask for header byte*/
+#define HEADER_COMMAND_MASK   0x80 /*!< Command mask for header byte*/
+
+#define LINEAR_FIFO_ADDRESS 0xFF  /*!< Linear FIFO address*/
+
+#define BUILT_HEADER(add_comm, w_r) (add_comm | w_r)  /*!< macro to build the header byte*/
+#define WRITE_HEADER    BUILT_HEADER(HEADER_ADDRESS_MASK, HEADER_WRITE_MASK) /*!< macro to build the write header byte*/
+#define READ_HEADER     BUILT_HEADER(HEADER_ADDRESS_MASK, HEADER_READ_MASK)  /*!< macro to build the read header byte*/
+#define COMMAND_HEADER BUILT_HEADER(HEADER_COMMAND_MASK, HEADER_WRITE_MASK) /*!< macro to build the command header byte*/
 
 StatusBytes RadioSpiWriteRegisters(uint8_t address, uint8_t n_regs, uint8_t *buffer) {
     static uint16_t tmpstatus;
