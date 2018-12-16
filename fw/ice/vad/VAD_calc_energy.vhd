d063e66f86a40d9269ea29dd559e62bd3c256409
David Frykskog <davfr422@student.liu.se>
2018-12-16 13:41:30 +0100

    Added basic VAD algorithm

--- fw/ice/vad/VAD_calc_energy.vhd
+++ fw/ice/vad/VAD_calc_energy.vhd
@@ -6,20 +6,17 @@ entity vad_energy is
 
 	generic (
 	
-		N : integer := 128
+		N : integer := 64
 	
 	);
 	
    port( 
 	
-		Clock         : in std_logic; -- Clock signal. 8 KHz in the moment of writing this.
-		Reset         : in std_logic; -- Reset signal. Reset module on '1'. 
-		Sample        : in signed(7 downto 0);
+		clock         : in std_logic; -- Clock signal. 8 KHz in the moment of writing this.
+		reset         : in std_logic; -- Reset signal. Reset module on '1'. 
+		sample        : in signed(7 downto 0);
 		
-		E_out         : out unsigned(15 downto 0);
-		
-		counter_debug : out unsigned(15 downto 0)
-		
+		e_out         : out unsigned(15 downto 0)
 
                   
 	);
@@ -27,35 +24,34 @@ end vad_energy;
 
 architecture arch_vad_energy of vad_energy is
 	
-	signal e_internal    : integer range 0 to 2**31 - 1 := 0;
-	signal counter       : integer range 0 to 2**7 := 0;
+	signal e_internal    : integer range 0 to 2**16 - 1 := 0;
+	signal counter       : integer range 0 to N      := 0;
 	
 begin
 
 	
-	CALC_VAD_ENERGY: process(Sample) is 
+	CALC_VAD_ENERGY: process(sample,clock,reset) is 
 	begin
 		
-		-- Go back to initial "state".
-		if (Reset = '1') then
+		if (reset = '1') then
 		
 			e_internal <= 0;
 			counter <= 0;
-			E_out <= (others => '0');
+			e_out <= (others => '0');
 
 		-- 
-		elsif ( rising_edge(Clock) ) then
+		elsif ( rising_edge(clock) ) then
 		
-			if counter >= N then
+			if counter = N then
 				
-				E_out <= to_unsigned( ( e_internal / N ) , 16 );
+				e_out <= to_unsigned( e_internal , 16 );
 				
-				e_internal <= ( to_integer(Sample) * to_integer(Sample) );
+				e_internal <= abs(to_integer(sample));
 				counter <= 1;
 				
 				
 			else
-				e_internal <= e_internal + ( to_integer(Sample) * to_integer(Sample) );
+				e_internal <= e_internal + abs(to_integer(sample));
 				counter <= counter + 1;
 				
 			end if;
@@ -64,7 +60,6 @@ begin
 		
 	end process;
 	
-	counter_debug <= to_unsigned(counter, 16);
 	
 
 end arch_vad_energy;
